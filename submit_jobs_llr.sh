3092f971d804f1e839ebc54ea9339b3a7d2f8f25
Jonas Rembser <jonas.rembser@cern.ch>
2020-06-08 16:08:05 +0200

    make submit_jobs_llr.sh copy production script for each job

--- submit_jobs_llr.sh
+++ submit_jobs_llr.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
 
+# 1 to 10 inclusive
 END=10
 for ((i=1;i<=END;i++)); do
     OUT_DIR=`printf %05d $i`
     mkdir $OUT_DIR
     cd $OUT_DIR
 
-    PILEUP_FILES_FILE=/grid_mnt/data_cms/rembser/production-test/pileup_files.txt
-    SCRIPT=/grid_mnt/data_cms/rembser/production-test/triboson-production
-    OUTPUT_DIR=/grid_mnt/data_cms/rembser/production-test/$OUT_DIR
+    PILEUP_FILES_FILE=/grid_mnt/data_cms/rembser/production-test-multithread/pileup_files.txt
+    SCRIPT=/grid_mnt/data_cms/rembser/production-test-multithread/triboson_production.sh
+    OUTPUT_DIR=/grid_mnt/data_cms/rembser/production-test-multithread/$OUT_DIR
+
+    cp $SCRIPT $OUTPUT_DIR/
 
     echo "#!/bin/bash
 
 cd $OUTPUT_DIR 
-touch hello_world.txt
-sh $SCRIPT -p $PILEUP_FILES_FILE -s WWW_dim8 -c -o $OUTPUT_DIR -n 1000" > submit_$OUT_DIR.sh
+sh triboson_production.sh -p $PILEUP_FILES_FILE -s WWW_dim8 -c -o $OUTPUT_DIR -n 1000" > submit_$OUT_DIR.sh
 
     t3submit submit_$OUT_DIR.sh
     sleep 1
